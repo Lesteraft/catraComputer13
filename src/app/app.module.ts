766a883c09a462dc17dbc50fd5658724ff7b1517
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-12-08 12:15:59 -0600

    1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InstruccionesComponent } from './components/instrucciones/instrucciones.component';
 import { RegistrosComponent } from './components/registros/registros.component';
 import { MensajesComponent } from './components/mensajes/mensajes.component';
 import { NavComponent } from './components/nav/nav.component';
 import { SubNavsComponent } from './components/sub-navs/sub-navs.component';
+import { InstruccionesComponent } from './components/instrucciones/instrucciones.component';
 
 @NgModule({
   declarations: [
