5dec7c943a469ab80a295dbf9d0dbce32fb14549
Carranza <allisoncarranza16@gmail.com>
2018-12-08 19:19:49 -0600

    Bifurcacion

--- decodificacion/prueba1.js
+++ decodificacion/prueba1.js
@@ -2,9 +2,9 @@
     var pc=0;
     var operacion=0;
     var memoria=0;
-    //prueba
-    var instruccionesArray = ['10007','10008','20007','30008',
-    '21009','11009','43000','00000','00000','00000'];
+    //prueba saltos
+    var instruccionesArray = ['10009','10010','20009','31010',
+    '41007','11009','43000','11010','43000','00000','00000'];
 
 
 while(pc!=-1) {     
@@ -52,15 +52,25 @@ while(pc!=-1) {
     break;
   case 33:
     acumulador = acumulador * parseInt(instruccionesArray[memoria]);
+    pc=pc+1;
     break;
   case 40:
-    code block
+    pc=parseInt(memoria);
     break;
   case 41:
-    code block
+      if(acumulador < 0){
+          pc=parseInt(memoria);
+      }else{
+          pc=pc + 1;
+      }
     break;
   case 42:
-    code block
+      if(acumulador == 0){
+          pc=parseInt(memoria);
+      }else{
+          pc=pc + 1;
+      }
+    
     break;
   case 43:
      window.alert('fin del programa');
@@ -70,5 +80,5 @@ while(pc!=-1) {
   default:
       window.alert('algo anda mal');
       pc=-1;
-}
+  }
 }
