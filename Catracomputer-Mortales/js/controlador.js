ae49ea423285f56c1af0c730598c3b35b99083bc
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-12-09 19:00:54 -0600

    1.5

--- Catracomputer-Mortales/js/controlador.js
+++ Catracomputer-Mortales/js/controlador.js
@@ -35,7 +35,7 @@ $("#instrucciones").keydown(
 
 $("#btnPlay").click(function() {
     console.log('click en play');
-
+    ejecutar($("#instrucciones").val());
 });
 
 $("#btnAtras").click(function() {
@@ -63,3 +63,46 @@ $(document).ready(function() {
     $("#infoPC").html(PC);
     $("#infoAC").html(acumulador);
 });
+
+function ejecutar(texto) {
+    var partes = texto.split("\n");
+    var accion;
+    var memoria;
+    console.log(partes);
+    while (PC != -1) {
+        if (validar(partes[PC], PC)) {
+            accion = partes[PC].substr(1, 2);
+            memoria = partes[PC].substr(3, 4, 5);
+            console.log(accion + " --> " + memoria);
+            if (PC < (partes.length - 1)) {
+                PC = PC + 1;
+            } else {
+                PC = -1;
+            }
+            $("#infoPC").html(PC);
+        } else {
+            PC = -1;
+        }
+    }
+    PC = 0;
+}
+
+function validar(parte, numero) {
+    var tamaño = parte.length;
+    if (tamaño != 6) {
+        alert('Error, instruccion incompleta: ' + (numero + 1));
+        return false;
+    }
+    if (parte.substr(0, 1) != '+') {
+        alert('Error, falta signo "+" en la instruccion número: ' + (numero + 1));
+        return false;
+    }
+    for (var i = 1; i < tamaño; i++) {
+        if (!(parte.charCodeAt(i) >= 48 && parte.charCodeAt(i) <= 57)) {
+            alert('Error, se ingresó caracter inválido en la instrucción: ' + (numero + 1));
+            return false;
+        }
+    }
+
+    return true;
+}
