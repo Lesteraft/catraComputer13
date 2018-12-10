b7e0ae1e48a2c6f90ba9147879798227ded0254f
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-12-10 01:58:55 -0600

    exito!

--- Catracomputer-Mortales/js/controlador.js
+++ Catracomputer-Mortales/js/controlador.js
@@ -4,7 +4,6 @@ var count;
 var lines;
 var PC = 0;
 var acumulador = 0;
-var textoLinea;
 
 function modificarDiv(numero) {
     $('#lineas').html(' ');
@@ -36,10 +35,8 @@ $("#instrucciones").keydown(
 );
 
 $("#btnPlay").click(function() {
-    console.log('click en play');
-    $("#notificaciones").html('');
     ejecutar($("#instrucciones").val());
-    PC = 0;
+    console.log('click en play');
 });
 
 $("#btnAtras").click(function() {
@@ -59,72 +56,44 @@ $("#btnSiguiente").click(function() {
 
 $("#btnPaso").click(function() {
     console.log('click en paso');
-    PC += 1;
-    ejecutarPasoAPaso();
+
 });
 
 $(document).ready(function() {
     console.log('LISTOOOOO');
     $("#infoPC").html(PC);
     $("#infoAC").html(acumulador);
-    memoria['009'] = 84;
 });
 
 function ejecutar(texto) {
     var partes = texto.split("\n");
     var accion;
-    var memoria;
-    console.log(partes);
-    do {
-        if (validar(partes[PC], PC)) {
-            accion = partes[PC].substr(1, 2);
-            memoria = partes[PC].substr(3, 4, 5);
-            accionEval(accion, memoria);
-            console.log(accion + " --> " + memoria);
-            if (PC < (partes.length - 1)) {
-                PC = PC + 1;
-            } else {
-                PC = -1;
-                break;
-            }
+    var memoriaOp;
+    for (var i = 0; i < partes.length; i++) {
+        if (validar(partes[i], i)) {
+            accion = partes[i].substr(1, 2);
+            memoriaOp = partes[i].substr(3, 4, 5);
+            accionEval(accion, memoriaOp);
+            console.log(accion + " --> " + memoriaOp);
             $("#infoPC").html(PC);
-        } else {
-            PC = -1;
-            break;
         }
-    } while (PC != 0 || PC != -1)
-    PC = 0;
-}
-
-function ejecutarPasoAPaso() {
-    text = $("#instrucciones").val();
-    lines = text.split('\n');
-    count = lines.length + 1;
-    textoLinea = lines[PC - 1];
-    if (PC < count && validar(textoLinea, PC - 1)) {
-        accion = lines[PC - 1].substr(1, 2);
-        memoria = lines[PC - 1].substr(3, 4, 5);
-        accionEval(accion, memoria);
-        console.log(accion + " --> " + memoria);
     }
+    PC = 0;
 }
 
 function validar(parte, numero) {
     var tamaño = parte.length;
     if (tamaño != 6) {
         alert('Error, instruccion incompleta: ' + (numero + 1));
-        PC = 0;
         return false;
     }
     if (parte.substr(0, 1) != '+') {
         alert('Error, falta signo "+" en la instruccion número: ' + (numero + 1));
-        PC = 0
         return false;
     }
     for (var i = 1; i < tamaño; i++) {
         if (!(parte.charCodeAt(i) >= 48 && parte.charCodeAt(i) <= 57)) {
             alert('Error, se ingresó caracter inválido en la instrucción: ' + (numero + 1));
-            PC = 0;
             return false;
         }
     }
@@ -135,95 +104,100 @@ function accionEval(accion, numero) {
     switch (accion) {
         case '10':
             {
+                var ingreso;
                 console.log("Lee");
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->10:</div><div class="col-7">Se LEE y ALMACENA en:</div><div class="col-2">${memoria}</div></div></div>`);
-                //Ojo, Alison, aquí debe ir relacionado con "consola" para que desde la consola el ususario ingrese lo que quiera
+                $("#notificaciones").append("--> 10: Se LEE y ALMACENA en: " + numero + '<br>');
+                memoria[numero] = prompt("Ingresa el dato");
+                console.log(memoria);
                 break;
             }
         case '11':
             {
                 console.log('Escribe');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->11:</div><div class="col-7">Se ESCRIBE e IMPRIME en pantalla</div></div></div>`);
+                $("#notificaciones").append("--> 11: Se ESCRIBE e IMPRIME en pantalla" + '<br>');
                 //aquí tambien a consola ahí se debe imprimir los resultados por ejempli
-                $("#consolaEntrada").css('display', 'none');
-                $("#consolaSalida").css('display', 'block');
-                $("#consolaSalida").html('A MOSTRAR: ' + memoria[numero]);
+                //$("#consolaEntrada").css('display', 'none');
+                $("#consolaSalida").css('display', 'flex');
+                $("#consolaSalidaDiv").html(memoria[numero]);
                 break;
             }
         case '20':
             {
                 console.log('Carga');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->20:</div><div class="col-7">Se CARGA y ACUMULA en:</div><div class="col-2">${memoria}</div></div></div>`);
+                $("#notificaciones").append("--> 20: Se CARGA y ACUMULA en: " + '<br>');
                 //se pedirá mediante "memoria" al arreglo de objetos para mandarlo al ACUMULADOR
+                acumulador = memoria[numero];
+                $("#infoAC").html(acumulador);
                 break;
             }
         case '21':
             {
                 console.log('Almacena');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->21:</div><div class="col-7">Se ALMACENA en:</div><div class="col-2">${memoria}</div></div></div>`);
+                $("#notificaciones").append("--> 21: Se ALMACENA en: " + '<br>');
                 //lo del ac irá al espacio de "memoria" nota: el número de memoria está de parametro
+                memoria[numero] = acumulador;
                 break;
             }
         case '30':
             {
                 console.log('Suma');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->30:</div><div class="col-7">Se SUMA y almacena en ACUMULADOR</div></div></div>`);
+                $("#notificaciones").append("--> 30: Se SUMA y almacena en ACUMULADOR");
                 //estas son funciones extras
                 break;
             }
         case '31':
             {
                 console.log('Resta');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->31:</div><div class="col-7">Se RESTA y almacena en ACUMULADOR</div></div></div>`);
+                $("#notificaciones").append("--> 31: Se RESTA y almacena en ACUMULADOR");
                 //estas son funciones extras
                 break;
             }
         case '32':
             {
                 console.log('Divide');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->32:</div><div class="col-7">Se DIVIDE y almacena en ACUMULADOR</div></div></div>`);
+                $("#notificaciones").append("--> 32: Se DIVIDE y almacena en ACUMULADOR");
                 //estas son funciones extras
                 break;
             }
         case '33':
             {
                 console.log('Multiplica');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->33:</div><div class="col-7">Se MULTIPLICA y almacena en ACUMULADOR</div></div></div>`);
+                $("#notificaciones").append("--> 33: Se MULTIPLICA y almacena en ACUMULADOR");
                 //estas son funciones extras
                 break;
             }
         case '40':
             {
                 console.log('Bifurca');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->40:</div><div class="col-7">BIFURCA a:</div><div class="col-2">${memoria}</div></div></div>`);
+                $("#notificaciones").append("--> 40: BIFURCA a: ");
                 //justo lo que hizo
                 break;
             }
         case '41':
             {
                 console.log('Bifurca si Negativo');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->41:</div><div class="col-7">BIFURCANEG a:</div><div class="col-2">${memoria}</div></div></div>`);
+                $("#notificaciones").append("--> 41: BIFURCANEG a: ");
                 //justo lo que hizo
                 break;
             }
         case '42':
             {
                 console.log('Bifurca si Cero');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->42:</div><div class="col-7">BIFURCACERO a:</div><div class="col-2">${memoria}</div></div></div>`);
+                $("#notificaciones").append("--> 42: BIFURCACERO a: ");
                 //justo lo que hizo
                 break;
             }
         case '43':
             {
                 console.log('Fin Programa');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->43:</div><div class="col-7">ALTO se culmina la TAREA</div></div></div>`);
+                $("#notificaciones").append("--> 43: ALTO se culmina la TAREA");
                 //justo lo que hizo
                 break;
             }
         default:
             {
                 console.log('error');
-                $("#notificaciones").append(`<div class="container"><div class="row"><div class="col-3">-->E/:</div><div class="col-7">Error de Instrucción</div></div></div>`);
+                $("#notificaciones").append("-->Error de Istrucción");
                 break;
             }
     }
